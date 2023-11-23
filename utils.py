e084db94e97cdd69a408a95bc99e98acf082d089
majuvi <mvil42@gmail.com>
2023-11-23 17:11:35 +0100

    Clean

--- utils.py
+++ utils.py
@@ -1,6 +1,12 @@
 import numpy as np
 import pandas as pd
 import datetime
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+from matplotlib.dates import date2num
+from itertools import cycle
+markers = ['o', '.', '^', 'v', '<', '>', '8', 's', 'p', '*', 'D']
+colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf']
 
 def match_recommendations_prices(fn_recommendations="analysis.csv", fn_prices="prices.csv", verbose=True):
     # Read recommendations
@@ -117,91 +123,6 @@ def add_information(recommendations, prices, extras=True, categories=True):
         recommendations['Vuosi'] = recommendations['Päivämäärä'].dt.year
 
     return(recommendations)
-
-def mcf_align(df_mcf1, df_mcf2):
-    # The event times may differ, reindex both to all event times
-    df_mcf1 = df_mcf1.set_index('Time')
-    df_mcf2 = df_mcf2.set_index('Time')
-    idx = df_mcf1.index.union(df_mcf2.index)
-    # Fill forward overlapping MCFs
-    # MCF1
-    df_mcf1 = df_mcf1.reindex(idx)
-    df_mcf1['dY'].fillna(value=0, inplace=True)
-    df_mcf1['dC'].fillna(value=0, inplace=True)
-    df_mcf1['Y'].ffill(inplace=True)
-    df_mcf1['Y'].fillna(0, inplace=True)
-    df_mcf1['C'].ffill(inplace=True)
-    df_mcf1.loc[df_mcf1['Y'] == 0, 'C'] = np.nan #end of MCF
-    df_mcf1.reset_index(inplace=True)
-    # MCF 2
-    df_mcf2 = df_mcf2.reindex(idx)
-    df_mcf2['dY'].fillna(value=0, inplace=True)
-    df_mcf2['dC'].fillna(value=0, inplace=True)
-    df_mcf2['Y'].ffill(inplace=True)
-    df_mcf2['Y'].fillna(0, inplace=True)
-    df_mcf2['C'].ffill(inplace=True)
-    df_mcf2.loc[df_mcf2['Y'] == 0, 'C'] = np.nan #end of MCF
-    df_mcf2.reset_index(inplace=True)
-    return(df_mcf1, df_mcf2)
-
-def mcf(fr_sample, sample='Sample', time='Time', events='dY', values='dC', extras=[],
-         include_lowest=False, bootstrap=None, save_bootstrap=False, level=0.05, add_cost=0.00):
-    df_sample = pd.DataFrame(fr_sample[sample])
-    # Calculate if the sample is observable at each time points
-    df_sample['Time'] = fr_sample[time]
-    df_sample['dY'] = fr_sample[events]
-    df_sample['Yi'] = df_sample.groupby(sample)['dY'].cumsum()
-    df_sample['Yi'] = df_sample.groupby(sample)['Yi'].shift(1).fillna(0)
-    # Whether to include the return at entry: left interval open or closed
-    df_sample['dY0i'] = (fr_sample[values].notnull() & (fr_sample[events] == 1)).astype(int)
-    if include_lowest:
-        df_sample['Yi'] = df_sample['Yi'] + df_sample['dY0i']
-    # Caclulate the return of the sample at each time point
-    df_sample['dC'] = df_sample['Yi'] * fr_sample[values]
-    for col in extras:
-        df_sample[col] = df_sample['Yi'] * fr_sample[col]
-    
-    if add_cost > 0.00:
-        df_sample.loc[fr_sample[events] > 0, 'dC'] = 0.0
-        df_sample.loc[fr_sample[events] != 0, 'dC'] -= add_cost
-    
-    # Calculate the total return and the total number of observable at each time point
-    df = df_sample.groupby('Time')[['dC', 'dY', 'dY0i'] + extras].agg('sum').sort_index().reset_index()
-    df['Y'] = df['dY'].cumsum().shift(1).fillna(0)
-    if include_lowest:
-        df['Y'] = df['Y'] + df['dY0i']
-    # MCF: Calculate the mean return and cumulative return at each time point 
-    df['dC'] = np.where(df['Y'] > 0, df['dC']/df['Y'], 0.0)
-    df['C'] = (1+df['dC']).cumprod()
-    for col in extras:
-        df[col] = np.where(df['Y'] > 0, df[col]/df['Y'], 0.0)
-    #df.drop('dY0i', axis=1, inplace=True)
-    df = df[['Time', 'dY', 'dC', 'Y', 'C'] + extras]
-    
-    # Recalculate MCFs from bootstrap replicates
-    if not bootstrap is None:
-        mcfs = {}
-        instances = fr_sample[sample].drop_duplicates()
-        for i in range(bootstrap):
-            # Take an equal size sample from original with replacement
-            fr_bootstrap = instances.sample(frac=1, replace=True)
-            fr_bootstrap['BootstrapSample'] = np.arange(len(fr_bootstrap))
-            fr_bootstrap = fr_bootstrap.merge(fr_sample)
-            # Calculate and save MCF from this sample
-            df_bootstrap = mcf(fr_bootstrap, sample='BootstrapSample', time=time, events=events, values=values,
-                               include_lowest=include_lowest, bootstrap=None, extras=extras)
-            _, df_bootstrap = mcf_align(df, df_bootstrap)
-            mcfs['C.{}'.format(i)] = df_bootstrap.set_index('Time')['C']
-        mcfs = pd.DataFrame(mcfs)
-        # Calculate quantiles
-        df['C Lower'] = mcfs.apply(lambda means: np.quantile(means, level/2), axis=1) 
-        df['C Upper'] = mcfs.apply(lambda means: np.quantile(means, 1 - level/2), axis=1) 
-        if save_bootstrap:
-            df = pd.concat((df, mcfs), axis=1)
-    
-    #extra_cols = [] if bootstrap is None else ['C Lower', 'C Upper']
-    return(df)
-
 
 def get_index_return(prices):
     # Some trading days are missing for a given stock. Maybe the trading was halted, no trade, or the data has problem
@@ -333,3 +254,214 @@ def bootstrap_mean(x):
     mean_uci = np.quantile(bootstrap_means, 0.975)
     return(pd.Series({'mean': mean, 'median': median, '95% lower': lci, '95% upper':uci, 
                       'mean 95% lower': mean_lci, 'mean 95% upper': mean_uci}))
+
+
+def mcf_align(df_mcf1, df_mcf2):
+    # The event times may differ, reindex both to all event times
+    df_mcf1 = df_mcf1.set_index('Time')
+    df_mcf2 = df_mcf2.set_index('Time')
+    idx = df_mcf1.index.union(df_mcf2.index)
+    # Fill forward overlapping MCFs
+    # MCF1
+    df_mcf1 = df_mcf1.reindex(idx)
+    df_mcf1['dY'].fillna(value=0, inplace=True)
+    df_mcf1['dC'].fillna(value=0, inplace=True)
+    df_mcf1['Y'].fillna(method='ffill', inplace=True)
+    df_mcf1['Y'].fillna(0, inplace=True)
+    df_mcf1['C'].fillna(method='ffill', inplace=True)
+    df_mcf1.loc[df_mcf1['Y'] == 0, 'C'] = np.nan #end of MCF
+    df_mcf1.reset_index(inplace=True)
+    # MCF 2
+    df_mcf2 = df_mcf2.reindex(idx)
+    df_mcf2['dY'].fillna(value=0, inplace=True)
+    df_mcf2['dC'].fillna(value=0, inplace=True)
+    df_mcf2['Y'].fillna(method='ffill', inplace=True)
+    df_mcf2['Y'].fillna(0, inplace=True)
+    df_mcf2['C'].fillna(method='ffill', inplace=True)
+    df_mcf2.loc[df_mcf2['Y'] == 0, 'C'] = np.nan #end of MCF
+    df_mcf2.reset_index(inplace=True)
+    return(df_mcf1, df_mcf2)
+
+def mcf_diff(fr_sample1, fr_sample2, 
+             sample1='Sample', time1='Time', events1='dY', values1='dC', 
+             sample2='Sample', time2='Time', events2='dY', values2='dC', 
+             include_lowest=False, bootstrap=None, save_bootstrap=False, level=0.05):
+    
+    df_mcf1 = mcf(fr_sample1, time=time1, sample=sample1, events=events1, values=values1, include_lowest=include_lowest)
+    df_mcf2 = mcf(fr_sample2, time=time2, sample=sample2, events=events2, values=values2, include_lowest=include_lowest)
+    df_mcf1, df_mcf2 = mcf_align(df_mcf1, df_mcf2)
+
+    # Calculate difference in returns and total observable
+    time = df_mcf1['Time'] # should be equal
+    dC_diff = df_mcf1['dC'] - df_mcf2['dC']
+    C_diff = df_mcf1['C'] - df_mcf2['C']
+    dY = df_mcf1['dY'] + df_mcf2['dY'] 
+    Y = np.minimum(df_mcf1['Y'], df_mcf2['Y'])
+    df_diff = pd.DataFrame({'Time': time, 'dY':dY, 'Y': Y, 'dC':dC_diff, 'C': C_diff})
+    
+    # Calculate MCF diffs from bootstrap replicates
+    if not bootstrap is None:
+        mcfs = {}
+        instances1 = fr_sample1[sample1].drop_duplicates()
+        instances2 = fr_sample2[sample2].drop_duplicates()
+        for i in range(bootstrap):
+            # Take an equal size samples from originals with replacement
+            fr_bootstrap1 = instances1.sample(frac=1, replace=True)
+            fr_bootstrap1['BootstrapSample'] = np.arange(len(fr_bootstrap1))
+            fr_bootstrap1 = fr_bootstrap1.merge(fr_sample1)
+            fr_bootstrap2 = instances2.sample(frac=1, replace=True)
+            fr_bootstrap2['BootstrapSample'] = np.arange(len(fr_bootstrap2))
+            fr_bootstrap2 = fr_bootstrap2.merge(fr_sample2)            
+            df_bootstrap = mcf_diff(fr_bootstrap1, fr_bootstrap2, include_lowest=include_lowest,
+                                    time1=time1, sample1='BootstrapSample', events1=events1, values1=values1,
+                                    time2=time2, sample2='BootstrapSample', events2=events2, values2=values2)
+            _, df_bootstrap = mcf_align(df_diff, df_bootstrap)
+            mcfs['C.{}'.format(i)] = df_bootstrap.set_index('Time')['C']
+        mcfs = pd.DataFrame(mcfs)
+        # Calculate quantiles
+        df_diff['C Lower'] = mcfs.apply(lambda means: np.quantile(means, level/2), axis=1) 
+        df_diff['C Upper'] = mcfs.apply(lambda means: np.quantile(means, 1 - level/2), axis=1)
+        if save_bootstrap:
+            df_diff = pd.concat((df_diff, mcfs), axis=1)
+        
+    return(df_diff)
+
+def mcf(fr_sample, sample='Sample', time='Time', events='dY', values='dC', extras=[],
+         include_lowest=False, bootstrap=None, save_bootstrap=False, level=0.05, add_cost=0.00):
+    df_sample = pd.DataFrame(fr_sample[sample])
+    # Calculate if the sample is observable at each time points
+    df_sample['Time'] = fr_sample[time]
+    df_sample['dY'] = fr_sample[events]
+    df_sample['Yi'] = df_sample.groupby(sample)['dY'].cumsum()
+    df_sample['Yi'] = df_sample.groupby(sample)['Yi'].shift(1).fillna(0)
+    # Whether to include the return at entry: left interval open or closed
+    df_sample['dY0i'] = (fr_sample[values].notnull() & (fr_sample[events] == 1)).astype(int)
+    if include_lowest:
+        df_sample['Yi'] = df_sample['Yi'] + df_sample['dY0i']
+    # Caclulate the return of the sample at each time point
+    df_sample['dC'] = df_sample['Yi'] * fr_sample[values]
+    for col in extras:
+        df_sample[col] = df_sample['Yi'] * fr_sample[col]
+    
+    if add_cost > 0.00:
+        df_sample.loc[fr_sample[events] > 0, 'dC'] = 0.0
+        df_sample.loc[fr_sample[events] != 0, 'dC'] -= add_cost
+    
+    # Calculate the total return and the total number of observable at each time point
+    df = df_sample.groupby('Time')[['dC', 'dY', 'dY0i'] + extras].agg('sum').sort_index().reset_index()
+    df['Y'] = df['dY'].cumsum().shift(1).fillna(0)
+    if include_lowest:
+        df['Y'] = df['Y'] + df['dY0i']
+    # MCF: Calculate the mean return and cumulative return at each time point 
+    df['dC'] = np.where(df['Y'] > 0, df['dC']/df['Y'], 0.0)
+    df['C'] = (1+df['dC']).cumprod()
+    for col in extras:
+        df[col] = np.where(df['Y'] > 0, df[col]/df['Y'], 0.0)
+    #df.drop('dY0i', axis=1, inplace=True)
+    df = df[['Time', 'dY', 'dC', 'Y', 'C'] + extras]
+    
+    # Recalculate MCFs from bootstrap replicates
+    if not bootstrap is None:
+        mcfs = {}
+        instances = fr_sample[sample].drop_duplicates()
+        for i in range(bootstrap):
+            # Take an equal size sample from original with replacement
+            fr_bootstrap = instances.sample(frac=1, replace=True)
+            fr_bootstrap['BootstrapSample'] = np.arange(len(fr_bootstrap))
+            fr_bootstrap = fr_bootstrap.merge(fr_sample)
+            # Calculate and save MCF from this sample
+            df_bootstrap = mcf(fr_bootstrap, sample='BootstrapSample', time=time, events=events, values=values,
+                               include_lowest=include_lowest, bootstrap=None, extras=extras)
+            _, df_bootstrap = mcf_align(df, df_bootstrap)
+            mcfs['C.{}'.format(i)] = df_bootstrap.set_index('Time')['C']
+        mcfs = pd.DataFrame(mcfs)
+        # Calculate quantiles
+        df['C Lower'] = mcfs.apply(lambda means: np.quantile(means, level/2), axis=1) 
+        df['C Upper'] = mcfs.apply(lambda means: np.quantile(means, 1 - level/2), axis=1) 
+        if save_bootstrap:
+            df = pd.concat((df, mcfs), axis=1)
+    
+    #extra_cols = [] if bootstrap is None else ['C Lower', 'C Upper']
+    return(df)
+
+def plot_mcf(fr, axs=None, label='', color='black', marker='.', drawstyle='steps-post',
+              CI=True, xmin=None, xmax=None, ymin=None, ymax=None):
+
+    # If no axis were given, create a new figure
+    if axs is None:
+        fig = plt.figure(figsize=(8,6))
+        gs = gridspec.GridSpec(2,1,height_ratios=[3,1])
+        ax = fig.add_subplot(gs[0])
+        axx = fig.add_subplot(gs[1], sharex=ax)
+    else:
+        ax, axx = axs
+
+    # Plot MCF
+    has_values, has_censor = fr['dC'] > 0, fr['dY'] < 0
+    ax.plot(fr.loc[has_values, 'Time'].values, fr.loc[has_values, 'C'].values, marker=marker, linestyle='', color=color, label=label)
+    ax.plot(fr.loc[has_censor, 'Time'].values, fr.loc[has_censor, 'C'].values, marker='+' if marker else '', linestyle='', color=color, label='')
+    ax.plot(fr['Time'].values, fr['C'].values, marker='', linestyle='-', drawstyle=drawstyle, color=color, label='')
+    if CI and ('C Lower' in fr) and ('C Upper' in fr):
+        ax.plot(fr['Time'].values, fr['C Lower'].values, marker='', linestyle='--', drawstyle=drawstyle, color=color, label='')
+        ax.plot(fr['Time'].values, fr['C Upper'].values, marker='', linestyle='--', drawstyle=drawstyle, color=color, label='')
+
+    ax.set_title('Mean Cumulative Function Plot')
+    ax.set_ylabel('E[C]')
+    if label:
+        ax.legend()
+    ax.grid(True)
+
+    # Plot Observable
+    axx.plot(fr['Time'].values, fr['Y'].values, drawstyle=drawstyle, color=color, label='')
+    axx.set_ylabel('Observable')
+    axx.set_xlabel('Time')
+    axx.grid()
+
+    # By default expand the plot to match the data
+    ax_xmin = fr['Time'].min()
+    ax_xmax = fr['Time'].max()
+    ax_ymin = (fr['C Lower'].min() if 'C Lower' in fr else fr['C'].min()) - 0.01
+    ax_ymax = (fr['C Upper'].max() if 'C Upper' in fr else fr['C'].max()) + 0.01
+    ax_yobsmin = fr['Y'].min() 
+    ax_yobsmax = fr['Y'].max()  + 1
+    # update
+    if not axs is None:
+        # mcf x axis
+        old_xmin, old_xmax = ax.get_xlim()
+        ax.set_xlim(ax_xmin, ax_xmax)
+        new_xmin, new_xmax = ax.get_xlim()
+        ax_xmin, ax_xmax = min(old_xmin, new_xmin), max(old_xmax, new_xmax)
+        # mcf y axis
+        old_ymin, old_ymax = ax.get_ylim()
+        ax.set_ylim(ax_ymin, ax_ymax)
+        new_ymin, new_ymax = ax.get_ylim()
+        ax_ymin, ax_ymax = min(old_ymin, new_ymin), max(old_ymax, new_ymax)
+        # observable
+        old_yobsmin, old_yobsmax = axx.get_ylim()
+        axx.set_ylim(ax_yobsmin, ax_yobsmax)
+        new_yobsmin, new_yobsmax = axx.get_ylim()
+        ax_yobsmin, ax_yobsmax = min(old_yobsmin, new_yobsmin), max(old_yobsmax, new_yobsmax)
+    # Overwrite if given
+    ax_xmin = xmin if not xmin is None else ax_xmin
+    ax_xmax = xmax if not xmax is None else ax_xmax
+    ax_ymin = ymin if not ymin is None else ax_ymin
+    ax_ymax = ymax if not ymax is None else ax_ymax
+    # set
+    ax.set_xlim(ax_xmin, ax_xmax)
+    ax.set_ylim(ax_ymin, ax_ymax)
+    axx.set_ylim(ax_yobsmin, ax_yobsmax)
+    
+    
+    return (ax,axx)
+
+def plot_mcfs(group_fr, axs=None, subplots=False, **kwargs):
+    # Plot timelines grouped by cohorts
+    idx, marker, color = 0, cycle(markers), cycle(colors)
+    for cohort, cohort_fr in group_fr:
+        mcf_kwargs = kwargs
+        if not 'color' in kwargs: mcf_kwargs = {**mcf_kwargs, **{'color': next(color)}}
+        if not 'marker' in kwargs: mcf_kwargs = {**mcf_kwargs, **{'marker': next(marker)}}
+        axs = plot_mcf(cohort_fr, axs, label=str(cohort) if cohort else '', **mcf_kwargs)
+        if subplots: axs = None
+
+    return axs
